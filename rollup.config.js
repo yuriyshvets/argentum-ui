a727c8bc3bbd08e4a8679b3b61d871c47457db21
Yurii Shvets <yurii.shvets@shijigroup.com>
2021-12-30 01:42:54 +0200

    Remove postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
@@ -29,7 +28,6 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
 
       terser(),
     ],
@@ -38,6 +36,5 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
   },
 ];
